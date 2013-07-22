cadff30609c0fa8ec4caba6bbdd05c047336f531
Jan Rubio <rubiojan@gmail.com>
2013-07-22 00:22:07 -0400

    Adding bell sound effects

---
+++ gw2audiolabs/public/javascripts/instrument.js
@@ -0,0 +1,93 @@
+(function ($, document, window, undefined) {
+    
+    'use strict';
+
+    // Name the plugin so it's only in one place
+    var pluginName = 'instrument';
+
+    // Default options for the plugin as a simple object
+    var defaults = {
+        path: '/notes/',
+        keyToNote: {
+            1: '_01_D',
+            2: '_02_E',
+            3: '_03_F',
+            4: '_04_G',
+            5: '_05_A',
+            6: '_06_B',
+            7: '_07_C',
+            8: '_08_D'
+        },
+        currentOctave: 1,
+        octaves: ['B', 'G', 'R']
+    };
+
+    // Instrument constructor
+    function Instrument(element, options) {
+        this.element = element;
+
+        this.options = $.extend({}, defaults, options);
+
+        this.$el       = $(element);
+        this.$el.data(name, this);
+
+        this._defaults = defaults;
+
+        var meta       = this.$el.data(name + '-opts');
+        this.opts      = $.extend(this._defaults, options, meta);
+        
+        this.opts.fullpath = this.opts.path + this.opts.name + '/';
+        
+        this.init();
+    }
+
+    Instrument.prototype = {
+        init: function () {
+            console.warn(this.opts.fullpath);
+        },
+        
+        play: function (key) {
+            var sound = this.opts.fullpath + 
+                        this.opts.octaves[this.opts.currentOctave] +
+                        this.opts.keyToNote[key],
+                variant = 4 + this.opts.currentOctave;
+                
+            if (key == 7 || key == 8) {
+                variant++;
+            }
+            
+            var tune = new Audio(sound + variant + '.mp3');
+            tune.play();
+        },
+        
+        upOctave: function () {
+            if (this.opts.currentOctave < this.opts.octaves.length - 1) {
+                this.opts.currentOctave++;
+            }
+        },
+        
+        downOctave: function () {
+            if (this.opts.currentOctave > 0) {
+                this.opts.currentOctave--;
+            }
+        },
+        
+        octave: function () {
+            return this.opts.currentOctave;
+        }
+    };
+
+    $.fn[pluginName] = function (options) {
+        // Iterate through each DOM element and return it
+        return this.each(function () {
+            // Prevent multiple instantiations
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Instrument(this, options));
+            }
+        });
+    };
+
+    var privateFunction = function () {
+    }
+
+}(jQuery, document, window));
---
+++ gw2audiolabs/public/javascripts/main.js
@@ -0,0 +1,75 @@
+(function ($) {
+    
+    'use strict';
+    
+    $(document).ready(function () {
+        var $octaveIndicator = $('.octave'),
+            octaves = ['low', 'medium', 'high'],
+            $buttons = [$('.b1'), $('.b2'), $('.b3'), $('.b4'), $('.b5'), 
+                        $('.b6'), $('.b7'), $('.b8'), $('.b9'), $('.b0')],
+            $bell = $('.bell')
+                        .instrument({ name: 'choirbell' })
+                        .data('plugin_instrument');
+        
+        $('body').on('keypress', function (e) {
+            console.warn('test');
+            
+            var code = e.which,
+                pressed = code - 48,
+                octave = 0;
+            
+            if (code > 48 && code < 57) {
+                $bell.play(pressed);
+                $buttons[pressed - 1].addClass('active');
+            } else if (code == 57) {
+                $bell.downOctave();
+            } else if (code == 48) {
+                $bell.upOctave();
+            }
+            
+            if (code == 57 || code == 48) {
+                octave = $bell.octave();
+                handleOctave(octave, $buttons[8], $buttons[9]);
+                handleOctaveIndicator(octaves[octave], $octaveIndicator);
+            }
+        });
+        
+        $('body').on('keyup', function () {
+            $.each($buttons, function (index, $button) {
+                $button.removeClass('active');
+            }); 
+        });
+        
+        $('.b1').on('click', function () { $bell.play(1); });
+        $('.b2').on('click', function () { $bell.play(2); });
+        $('.b3').on('click', function () { $bell.play(3); });
+        $('.b4').on('click', function () { $bell.play(4); });
+        $('.b5').on('click', function () { $bell.play(5); });
+        $('.b6').on('click', function () { $bell.play(6); });
+        $('.b7').on('click', function () { $bell.play(7); });
+        $('.b8').on('click', function () { $bell.play(8); });
+        
+        $('.b9').on('click', function () { $bell.downOctave(); handleOctaveIndicator(octaves[$bell.octave()], $octaveIndicator); handleOctave($bell.octave(), $buttons[8], $buttons[9]); });
+        $('.b0').on('click', function () { $bell.upOctave();handleOctaveIndicator(octaves[$bell.octave()], $octaveIndicator); handleOctave($bell.octave(), $buttons[8], $buttons[9]); });
+    });
+    
+    function handleOctave(octave, $up, $down) {
+        if (octave === 0) {
+            $up.addClass('off');
+        } else if (octave === 2) {
+            $down.addClass('off');
+        } else {
+            $down.removeClass('off');
+            $up.removeClass('off');
+        }        
+    }
+    
+    function handleOctaveIndicator(octave, $indicator) {
+        $indicator
+            .removeClass('high')
+            .removeClass('medium')
+            .removeClass('low');
+        $indicator.addClass(octave);
+    }
+    
+}(jQuery));
